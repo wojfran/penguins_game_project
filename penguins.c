eab796754cf47aa2dd9c7c107b34ea6b136029bb
fwojcik <f.wojcik@stud.elka.pw.edu.pl>
2022-11-09 13:51:44 +0100

    Created the file containing the code for the project called penguins.c

---
+++ penguins.c
@@ -0,0 +1,11 @@
+int x, y; // board dimensions
+int penguins; // number of penguins
+int players; // numebr of players
+int current_player; //integer signifying the current player
+
+// here create empty functions which will accomplis the various tasks in the flowchart
+
+
+int main(){
+
+}
