a2e5448807606f6fd8fbeeaf5ffe9da791d49a7e
Franciszek Wójcik <f.wojcikk@gmail.com>
2022-11-09 17:01:10 +0100

    Added a few empty functions from the flowchart which will be expanded later

--- penguins.c
+++ penguins.c
@@ -3,8 +3,46 @@ int penguins; // number of penguins
 int players; // numebr of players
 int current_player; //integer signifying the current player
 
-// here create empty functions which will accomplis the various tasks in the flowchart
+// here create empty functions which will accomplish the various tasks in the flowchart
+void input_board_dimensions() {
 
+}
+
+void input_number_of_players_and_penguins() {
+
+}
+
+void generate_board(){
+
+}
+
+void set_current_player(){
+
+}
+
+bool check_if_current_player_has_penguins_in_hand(){
+
+}
+
+bool check_if_space_on_the_board(){
+
+}
+
+void show_borad(){
+
+}
+
+void ask_for_coordinates(){
+
+}
+
+void place_penguin(int x, int y){
+
+}
+
+void update_score(int player, int score){
+
+}
 
 int main(){
 
