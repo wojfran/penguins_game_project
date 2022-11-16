a938d9303454aeb8834f7c1199e4a082336dc5e3
ayaman <01175924@pw.edu.pl>
2022-11-16 21:58:16 +0100

    input.h

--- input.h
+++ input.h
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+
+void input_board_dimensions() {
+
+} // a function which asks players for board dimensions(length and width)
+
+int check_if_input_is_correct(){
+
+} // this function checks the written input
+
+void input_number_of_players_and_penguins() {
+    if (check_if_input_is_correct()){
+
+    } else {
+
+    }
+} // used for determining numbers of players and penguins
+
+int are_coordinates_valid() {
+
+} // checks if the specified coordinates for a penguin to place are valid
+
+void ask_for_coordinates(){
+
+
+    if (are_coordinates_valid()){
+
+    } else {
+
+    }
+} // this function lets user to enter the coordinates that they want to move
+
+void ask_for_penguin_coordinates(){
+
+    if (does_penguin_belong_to_current_player() && are_coordinates_valid()){
+
+    } else {
+
+    }
+} // it supposed to let user to specify coordinates where they want to move their penguins
