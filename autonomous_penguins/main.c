bc047bb23984fa33dd0e24ee2c2e2d5fb18b097a
Franciszek Wójcik <f.wojcikk@gmail.com>
2023-01-18 18:14:31 +0100

    Decided to leave comments in the main of the autonomous_mode to increase clarity

--- autonomous_penguins/main.c
+++ autonomous_penguins/main.c
@@ -35,8 +35,10 @@ int main(int argc, char* argv[]){
         if (mode == 1 || mode == 2) {
             
             if (mode == 1) {
+                // reading maximum amount of penguins per player
                 penguins = read_commandline_penguin_amount(argv[2]);
 
+                // checking if the amount of penguins is correct
                 if (penguins == -1) {
                     printf("The amount of penguins could not be read.\n");
                     return 3;
@@ -45,6 +47,7 @@ int main(int argc, char* argv[]){
                 printf("The read amount of penguins is: \n%d\n\n", penguins);
             }
 
+            // reading the input text file
             FILE* input;
             if (mode == 1) input = fopen(argv[3], "r");
             else if (mode == 2) input = fopen(argv[2], "r");
@@ -53,25 +56,39 @@ int main(int argc, char* argv[]){
                 return 3;
             }
                 
+
+            // checking if the fopen function was successful
+            // ie. if file could be read
             if (input == NULL) {
                 printf("Input board file could not be opened.\n");
                 return 3;
             }
                 
+
+            // reading the dimensions of the board 
+            // as well as checking if they were properly read
             if (read_dimensions(input)) {
                 printf("The following dimension were read:\n"
                 "Rows(y): %d\nColumns(x): %d\n\n", rows, columns);
             } else return 2;
                 
+            // allocating memory for the board to be read
             int** board = allocate_memory_for_board(columns, rows);
 
+            // generating the board values from the input file
+            // as well as checking if they were properly read
+            // and no value was outside of the permitted range
             if(read_board_values_from_file(input, board)) {
                 printf("Such a board was read:\n");
                 display_board_file_format(board, rows, columns);
             } else return 2;
                 
+            // allocating memory for the players to be generated from file
             Player* players = players = malloc(10 * sizeof(Player));
                 
+            // generating players out of file data
+            // as well as checking if they are valid
+            // ie. if there arent't any duplicates
             int check = read_players_from_file(input, players);
             if(check == 1 || mode == 1) {
                 printf("\nThe following players were read from file:\n");
@@ -88,6 +105,9 @@ int main(int argc, char* argv[]){
 
             fclose(input);
 
+            // reading penguins from board, assigning them
+            // the values of their coordinates as well as
+            // assigning them to their respective players
             if (read_penguins_from_board(board, players)) {
                 printf("\nThe read penguins have the following coordinates: \n");
                 print_all_players_penguin_coordinates(players);
@@ -95,18 +115,23 @@ int main(int argc, char* argv[]){
             } else return 3;
 
             if (mode == 1) {
+                // check if the player with our id name has penguins
+                // to be placed and if so ask for coordinates, then
+                // place penguin. if we have already placed our penguins
+                // returns 1
                 if (check_if_current_players_penguins_can_be_placed(players[current_player])){
                     display_board_file_format(board, rows, columns);
-                    printf("\n*\n");
                     automatic_placement_chess_method(players[current_player], board);
-                    printf("\n*\n");
-                    
                     place_penguin(&players[current_player], board);
                 } else {
                     printf("We have already placed all our penguins. ");
                     return 1;
                 }
             } else if (mode == 2) {
+                // check if the player with our id name has penguins
+                // to be placed and if so ask for coordinates, then
+                // place penguin. if we have already placed our penguins
+                // returns 1
                 if (check_if_current_players_penguins_can_move(players[current_player], board)){
                     display_board_file_format(board, rows, columns);
                     automatically_pick_movement_coordinates(players[current_player], board);
@@ -129,7 +154,9 @@ int main(int argc, char* argv[]){
                 printf("Invalid mode. \n");
                 return 3;
             }
-
+            
+            // checking if the fopen function was successful
+            // ie. if file could be read
             if (output == NULL) {
                 printf("Output board file could not be opened.\n");
                 return 3;
@@ -138,7 +165,7 @@ int main(int argc, char* argv[]){
             write_game_state_to_output_file(output, board, players);
 
             fclose(output);
-            deallocate_players(players, player_number);    
+            if (mode == 1) deallocate_players(players, player_number);    
             free_board_memory(board, columns);
             return 0;
 
